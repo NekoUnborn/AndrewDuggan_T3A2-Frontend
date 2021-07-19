2beed31953c7e396e217f49a92fb69fd5990c9a0
Timothy Moss <tim@Timothys-MacBook-Pro.local>
2021-07-19 12:48:00 +1000

    More work done on the addiing function for medicine, form now takes data and posts it to the rails api

--- src/Components/Medication/AddMedication.jsx
+++ src/Components/Medication/AddMedication.jsx
@@ -1,15 +1,27 @@
-const AddMedication = () => {
+const AddMedication = (data) => {
+    async function addMedicine(){
+        const res = await fetch('http://localhost:4000/api/v1/medicines', {method: 'POST', body: data})
+    }
+    function submit(e){
+        e.preventDefault()
+        const data = {
+            name: e.target.elements.medicine.value,
+            description: e.target.elements.description.value
+        }
+        addMedicine(data)
+        console.log('Completed Request')
+    }
     return (
         <div>
             <h1> Made it here</h1>
-            <form>
+            <form onSubmit={submit}>  
                 <label>Name: </label>
-                <input type='text'></input>
+                <input type='text' name='medicine'></input>
                 <label>Description</label>
                 <br></br>
-                <textarea type='text'></textarea>
+                <textarea type='text' name='description'></textarea>
                 <br></br>
-                <button type="submit" onSubmit={console.log('Submitted the form')}>Add Medication</button>
+                <button type="submit" >Add Medication</button>
             </form>
         </div>
     )
