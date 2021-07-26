cabeb4786b72e11348c503c9fa8e42771f645a44
Timothy Moss <tim@timothys-mbp.lan>
2021-07-26 22:57:54 +1000

    Work done to prevent rewards from being repeatedly added

--- src/components/rewards/Rewards.jsx
+++ src/components/rewards/Rewards.jsx
@@ -3,13 +3,15 @@ import { stateContext } from "../../stateReducer"
 const Rewards = (props) => {
     const id = props.match.params.id
     const context = useContext(stateContext)
+    console.log(id)
     const [rewards, setRewards] = useState()
     async function getRewards () {
-        const res = fetch(`${process.env.REACT_APP_API_ENDPOINT}child/${id}/rewards`, {
+        const res = await fetch(`${process.env.REACT_APP_API_ENDPOINT}child/${id}/rewards`, {
             headers: {
             Authorization: `Bearer ${context.token}`
           }})
-          const data = await (await res).json()
+          const data = await res.json()
+          console.log(data)
           setRewards(data)
     }
     useEffect(() =>{
@@ -19,11 +21,11 @@ const Rewards = (props) => {
         <div>
             <h1>Rewards</h1>
             <h1>{id}</h1>
-            {rewards.map((reward, index) => {
+            {rewards ? rewards.map((reward, index) => {
                 return (
                 <p key={index}>{reward.date}</p>
                 )
-            })}
+            }) : <p>Nothinjg here</p>}
         </div>
     )
 }
