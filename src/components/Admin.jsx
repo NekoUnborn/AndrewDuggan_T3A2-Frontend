74bd8c3b8ab0398876a96beabeba67603cd6205e
Timothy Moss <tim@timothys-mbp.lan>
2021-07-27 21:14:04 +1000

    Not sure why but deleting a certain component not used anywhere bricks the frontend

---
+++ src/components/Admin.jsx
@@ -0,0 +1,13 @@
+
+import { AdminNav } from "./StylingComponents/StyledComponents"
+const Admin = () => {
+    return (
+        <AdminNav>
+            {/* <NavItem><Link style={style} id='home' to="/">Home</Link></NavItem> */}
+            {/* <li><Link id='child' to="/child" id='testchild'>Children</Link></li> */}
+            {/* <NavItem><Link style={style} id='medicine' to='/medicine'>Medicine</Link></NavItem> */}
+        </AdminNav>
+    )
+}
+
+export default Admin
